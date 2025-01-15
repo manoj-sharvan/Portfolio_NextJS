e6625ff9ac9a24caee146fff02c995e23967cef4
Manoj S <73774886+manoj-sharvan@users.noreply.github.com>
2025-01-15 21:17:44 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg dark:text-light dark:border-light sm:text-base'>
       <Layout className='py-8 flex items-center justify-between lg:flex-col lg:py-6'>
-        <span>{new Date().getFullYear()}&copy; All Rights Reserved</span>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className='flex items-center lg:py-2'>
           Build using
           <span className='w-6 mx-1'>
@@ -16,7 +16,7 @@ const Footer = () => {
             Manoj S
           </Link>
         </div>
-        <Link href={'/'} className='underline underline-offset-2 ml-2'>
+        <Link href={'mailto:manojsharvan@gmail.com'} className='underline underline-offset-2 ml-2'>
           Say Hello
         </Link>
       </Layout>
