ef192109a8077004c12786708b24febc778f9523
Manoj S <73774886+manoj-sharvan@users.noreply.github.com>
2025-01-15 21:21:57 +0530

    Update Hire.js

--- src/Components/Hire.js
+++ src/Components/Hire.js
@@ -12,7 +12,7 @@ const Hire = () => {
       </div>
       {/* <Link href={"mailto:manojsharvan98@gmail.com"} className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid boder-dark w-20 h-20 rounded-full font-semibold  hover:bg-light hover:text-dark transition duration-150 ease-in">Hire Me</Link> */}
       <Link
-        href={'mailto:manojsharvan98@gmail.com'}
+        href={'mailto:manojsharvan@gmail.com'}
         className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid boder-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark transition duration-150 ease-in dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px] '
       >
         Contact
