68b4354dfc7fde274a1679b9a3b52011d25a8668
Manoj S <73774886+manoj-sharvan@users.noreply.github.com>
2025-01-16 06:49:05 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,17 @@
 /* eslint-disable react/no-unescaped-entities */
-import AnimatedText from '@/Components/AnimatedText';
-import Layout from '@/Components/Layout';
-import Head from 'next/head';
-import React, { useEffect, useRef } from 'react';
-import profile from '../../public/images/profile/developer-pic-2.jpg';
-import Image from 'next/image';
-import { useInView, useMotionValue, useSpring } from 'framer-motion';
-import Skills from '@/Components/Skills';
-import Experience from '@/Components/Experience';
-import Education from '@/Components/Education';
+import AnimatedText from "@/Components/AnimatedText";
+import Layout from "@/Components/Layout";
+import Head from "next/head";
+import React, { useEffect, useRef } from "react";
+import profile from "../../public/images/profile/developer-pic-2.jpg";
+import Image from "next/image";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/Components/Skills";
+import Experience from "@/Components/Experience";
+import Education from "@/Components/Education";
 import TransitionEffect from "@/Components/TransitionEffect";
 
-const AnimatedNumbers = ({ value, isDecimal }) => {
+const AnimatedNumbers = ({ value, isDecimal, isAdding }) => {
   const ref = useRef(null);
   const motionValue = useMotionValue(0);
   const springValue = useSpring(motionValue, { duration: 3000 });
@@ -24,12 +24,16 @@ const AnimatedNumbers = ({ value, isDecimal }) => {
   }, [isInView, value, motionValue]);
 
   useEffect(() => {
-    springValue.on('change', (latest) => {
+    springValue.on("change", (latest) => {
       if (ref.current && latest <= value) {
         if (isDecimal) {
           ref.current.textContent = latest.toFixed(1);
         } else {
-          ref.current.textContent = latest.toFixed(0);
+          if (isAdding) {
+            ref.current.textContent = `${latest.toFixed(0) + "" + "+"}`;
+          } else {
+            ref.current.textContent = latest.toFixed(0);
+          }
         }
       }
     });
@@ -49,108 +53,116 @@ const calculateYearsOfExperience = (careerStartDate) => {
   return yearsOfExperience.toFixed(1);
 };
 
-const careerStartDate = new Date('2022-01-10');
+const careerStartDate = new Date("2022-01-10");
 const yearsOfExperience = Number(calculateYearsOfExperience(careerStartDate));
-
 
 const about = () => {
   return (
     <>
       <Head>
         <title>Manoj | About Page</title>
-        <meta name='description' content={`Hi, I'm Manoj, a Front end developer with a passion for creating
+        <meta
+          name="description"
+          content={`Hi, I'm Manoj, a Front end developer with a passion for creating
                 beautiful, functional, and user-centered digital experiences.
                 With ${yearsOfExperience} years of experience in the field. I am always looking
                 for new and innovative ways to bring my clients' visions to
-                life.`} />
+                life.`}
+        />
       </Head>
       <TransitionEffect />
       <main
-        className='flex w-full flex-col items-center justify-center text-dark dark:text-light'
+        className="flex w-full flex-col items-center justify-center text-dark dark:text-light"
         role="main"
         aria-labelledby="main-heading"
       >
         <h1 id="main-heading" className="sr-only">
           About Me
         </h1>
-        <Layout className='pt-16'>
+        <Layout className="pt-16">
           <AnimatedText
-            text='Passion Fuels Purpose! '
-            className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl '
+            text="Passion Fuels Purpose! "
+            className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl "
             aria-label="Passion Fuels Purpose"
           />
-          <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
-            <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2  md:col-span-8'>
-              <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2  md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
                 Biography
               </h2>
-              <div className='biography-content' tabIndex={0}>
-                <p className='font-medium'>
-                  Hello, I'm Manoj, a web developer who is also well-versed
-                  in accessibility practices. With over {yearsOfExperience} years of experience,
-                  my goal is to craft visually appealing, functional, and
-                  inclusively designed digital experiences that can be
-                  seamlessly accessed by blind users.
+              <div className="biography-content" tabIndex={0}>
+                <p className="font-medium">
+                  Hello, I'm Manoj, a web developer who is also well-versed in
+                  accessibility practices. With over {yearsOfExperience} years
+                  of experience, my goal is to craft visually appealing,
+                  functional, and inclusively designed digital experiences that
+                  can be seamlessly accessed by blind users.
                 </p>
-                <p className='my-4 font-medium'>
-                  I possess a deep understanding of accessibility standards, ensuring
-                  that my creations are usable and enjoyable for everyone, including
-                  those who rely on screen readers and assistive technologies.
-                  In my view, design extends beyond aesthetics – it
-                  involves problem-solving and creating seamless, enjoyable
-                  interactions for users.
+                <p className="my-4 font-medium">
+                  I possess a deep understanding of accessibility standards,
+                  ensuring that my creations are usable and enjoyable for
+                  everyone, including those who rely on screen readers and
+                  assistive technologies. In my view, design extends beyond
+                  aesthetics – it involves problem-solving and creating
+                  seamless, enjoyable interactions for users.
                 </p>
-                <p className='font-medium'>
-                  Whether I'm immersed in a website project or championing accessibility,
-                  I approach each endeavor with enthusiasm. I'm excited about the chance to
-                  contribute my expertise and enthusiasm to your upcoming project, making
-                  sure that it's not only visually stunning but also technically accessible.
+                <p className="font-medium">
+                  Whether I'm immersed in a website project or championing
+                  accessibility, I approach each endeavor with enthusiasm. I'm
+                  excited about the chance to contribute my expertise and
+                  enthusiasm to your upcoming project, making sure that it's not
+                  only visually stunning but also technically accessible.
                 </p>
               </div>
             </div>
-            <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
+            <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
               {/* Decorative Background */}
               <div
-                className='absolute top-0 -right-3  -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light '
+                className="absolute top-0 -right-3  -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light "
                 aria-hidden="true"
               />
               {/* Profile Image */}
               <Image
                 src={profile}
-                alt='Manoj S - Web Developer'
-                className='w-full h-auto rounded-2xl'
+                alt="Manoj S - Web Developer"
+                className="w-full h-auto rounded-2xl"
                 priority
-                sizes='(max-width: 768px) 100vw,
+                sizes="(max-width: 768px) 100vw,
     (max-width: 1200px) 50vw,
-    33vw'
+    33vw"
               />
             </div>
 
-            <div className='col-span-2 flex flex-col items-end justify-around xl:col-span-8 xl:flex-row xl:items-center md:order-3'>
-              <div className='flex flex-col items-end justify-center xl:items-center'>
+            <div className="col-span-2 flex flex-col items-end justify-around xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
                 <span
-                  className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'
+                  className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl"
                   aria-live="polite"
                 >
-                  <AnimatedNumbers value={5} isDecimal={false} />
+                  <AnimatedNumbers
+                    value={10}
+                    isDecimal={false}
+                    isAdding={true}
+                  />
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
                   <span id="projects-completed-label">Projects Completed</span>
                 </h2>
               </div>
-              <div className='flex flex-col items-end justify-center xl:items-center'>
+              <div className="flex flex-col items-end justify-center xl:items-center">
                 <span
-                  className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'
+                  className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl"
                   aria-live="polite"
                 >
                   <AnimatedNumbers value={yearsOfExperience} isDecimal={true} />
                 </span>
-                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm'>
-                  <span id="years-of-experience-label">Years of Experience</span>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center md:text-lg sm:text-base xs:text-sm">
+                  <span id="years-of-experience-label">
+                    Years of Experience
+                  </span>
                 </h2>
               </div>
             </div>
-
           </div>
           {/* Skills Section */}
           <div aria-labelledby="skills-heading">
